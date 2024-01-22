ef59da3f2e769af01b1822bb45cb8d42a669d7f4
Simon Dutka <Szymon.dutka@outlook.com>
2024-01-22 16:10:25 +0100

    Added getBrainFuck()

--- utilities/getBrainFuck.js
+++ utilities/getBrainFuck.js
@@ -1,4 +1,42 @@
+let output = '';
+
 const getBrainFuck = (multiples) => {
+    for (let i = 0; i < multiples.length; i++) {
+        const symbols = ['>', '<', '+', '-', '[', ']', '.'];
+
+        let loopValue = '',
+            incrementValue = '',
+            startPointer = '',
+            differentValue = '';
+
+        let numberOfCharacter = i + 1;
+
+        startPointer += symbols[1].repeat(i);
+
+        loopValue += symbols[2].repeat(multiples[i].firstDigit);
+
+        incrementValue +=
+            '[' +
+            symbols[0].repeat(numberOfCharacter) +
+            symbols[2].repeat(multiples[i].secondDigit) +
+            symbols[1].repeat(numberOfCharacter) +
+            '-]';
+
+        if (multiples[i].different !== 0) {
+            differentValue +=
+                symbols[0].repeat(numberOfCharacter) +
+                symbols[2].repeat(multiples[i].different);
+        }
+
+        output +=
+            startPointer +
+            loopValue +
+            incrementValue +
+            differentValue +
+            symbols[6];
+    }
+
+    return output;
 };
 
 export default getBrainFuck;
